6bd754aba90f85564a5f5c8e175190189d2dc310
louie_gomez <kinggomez027.lg@gmail.com>
2022-08-31 17:37:57 -0400

    stocks list

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
 
 function MainContainer() {
+  const [stocks, setStocks] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/stocks")
+    .then((r) => r.json())
+    .then((stocks) => setStocks(stocks))
+  }, []);
+
+
   return (
     <div>
       <SearchBar />
--- src/components/StockContainer.js
+++ src/components/StockContainer.js
@@ -1,15 +1,7 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
 import Stock from "./Stock";
 
 function StockContainer() {
-  const [stocks, setStocks] = useState([]);
-
-  useEffect(() => {
-    fetch("http://localhost:3001/stocks")
-    .then((r) => r.json())
-    .then((stocks) => setStocks(stocks))
-  }, []);
-
   return (
     <div>
       <h2>Stocks</h2>
