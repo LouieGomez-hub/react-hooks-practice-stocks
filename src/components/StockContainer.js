ec6e8b174d4c6d9ae54fc0aded92f391591af6ee
louie_gomez <kinggomez027.lg@gmail.com>
2022-08-31 17:33:16 -0400

    stocks list

--- src/components/StockContainer.js
+++ src/components/StockContainer.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Stock from "./Stock";
 
 function StockContainer() {
+  const [stocks, setStocks] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/stocks")
+    .then((r) => r.json())
+    .then((stocks) => setStocks(stocks))
+  }, []);
+
   return (
     <div>
       <h2>Stocks</h2>
